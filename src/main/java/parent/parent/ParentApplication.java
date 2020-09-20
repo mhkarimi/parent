30c3b2e59dc3b1b8769cb835c698f96a1bf46606
mhkarimi <mh2821945>
2020-09-20 12:36:02 +0430

    first commit after pulling child

--- src/main/java/parent/parent/ParentApplication.java
+++ src/main/java/parent/parent/ParentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParentApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello World from the parent project");
         SpringApplication.run(ParentApplication.class, args);
     }
 
